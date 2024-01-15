dfac6e8792a5262a0ae69e7c74f45fd795247569
jnc-explosion <himajin8634@gmail.com>
2024-01-16 03:34:49 +0900

    物語のイントロを追加

--- src/game/Game.d
+++ src/game/Game.d
@@ -7,17 +7,19 @@ alias Router = _Router!int;
 alias RouteObject = _RouteObject!int;
 
 enum Routes {
-  Home, Title, Editor,
+  Home, Title, Editor, GameOver, Abstract,
 }
 
 class Game: GameObject {
   Router router;
 
   override void setup() {
-    router = register(new Router(Routes.Title, [
+    router = register(new Router(Routes.Abstract, [
       Routes.Title: new TitleScene(),
       Routes.Home: new HomeScene(),
       Routes.Editor: new EditorScene(),
+      Routes.GameOver: new GameOverScene(),
+      Routes.Abstract: new AbstractScene(),
     ]));
     //register(new HomeScene);
     //register(new GameOverScene);
--- src/game/entities/package.d
+++ src/game/entities/package.d
@@ -11,4 +11,5 @@ public import game.entities.Missile;
 public import game.entities.Point;
 public import game.entities.TimeLimitClock;
 public import game.entities.UI;
-public import game.entities.Title;
+public import game.entities.TextBox;
+public import game.entities.ImageBox;
